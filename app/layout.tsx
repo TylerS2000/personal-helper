8e70343aa068d601a81fc2d211bfd3220ed7b065
TylerS2000 <tylerschulten@gmail.com>
2025-12-23 18:03:39 -0500

    learning next.js, pb impl

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <nav>
+          <ul className="flex gap-4 m-4">
+            <Link href="/">Home</Link>
+            <Link href="/vision">Vision</Link>
+          </ul>
+        </nav>
         {children}
       </body>
     </html>
---
+++ app/vision/page.tsx
@@ -0,0 +1,8 @@
+export default function Vision() {
+    return (
+        <div>
+            <h1>Vision Board</h1>
+            <p>Here is my vision board</p>
+        </div>
+    );
+}
